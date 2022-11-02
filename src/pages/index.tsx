ba9bd7278d8c2a9e3ffd2a49aa3a89ff4e60594a
Anurag30112003 <anuragsharma011011@gmail.com>
2022-11-03 01:51:48 +0530

    cleanup

---
+++ src/pages/index.tsx
@@ -0,0 +1,20 @@
+import Head from 'next/head'
+
+
+export default function Home() {
+  return (
+    <div className="">
+      <Head>
+        <title>borrowIT</title>
+        <meta name="description" content="BorrowIt" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className="">
+        <div>
+          Hello World
+        </div>
+       </main>
+    </div>
+  )
+}
